7b341511b4ab6baa035569de8bac2fe3e3740c28
Martin Henriksen <martin.henriksen.92@gmail.com>
2022-10-31 22:04:37 +0100

    Chore: poc test fluent call

--- tests/Feature/GetFluentTest.php
+++ tests/Feature/GetFluentTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Services\FluentService;
 use Illuminate\Http\Response;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 class GetFluentTest extends TestCase
@@ -11,6 +13,11 @@ class GetFluentTest extends TestCase
     /** @test */
     public function test_get_fluent_resolve()
     {
+        $this->mock(FluentService::class, function (MockInterface $mock) {
+            $mock->shouldReceive('getRepository->getFluentObject->resolve')
+                ->andReturn('fluent');
+        });
+
         $response = $this->get(route('fluent.index'));
 
         $response->assertStatus(Response::HTTP_OK)
